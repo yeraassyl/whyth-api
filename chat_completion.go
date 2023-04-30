49a7e94a5fe550439dfcd5f1e7ca542fe530973d
yeraassyl <yerassyl88@gmail.com>
2023-05-01 02:19:21 +0600

    remove unnecessary comments

--- chat_completion.go
+++ chat_completion.go
@@ -132,21 +132,3 @@ func (cc *ChatCompletion) CreateSession(lessonID, sessionID, username string) (*
 	}
 	return response, err
 }
-
-// how teacher will access the completion?
-// need to save the teachers preset in redis
-// how do I know which teacher is for which session?
-// should teacher register? how should I limit the teachers and students?
-// so basically, do you choose which do you want, teacher or students
-// if teacher then you can create a lesson, preset some params and then send a link to your students.
-// the students will login using a code, or by link you can say. what does this link will do?
-
-// vot esli prosto est' service kuda mozhno pisat' i otvet poluchat', smysla net delat' takoe potomu chto est' uzhe chatgpt
-// est' smysl sdelat' AI assitentom dlya uchitelya i dlya studenta.
-// sil'no uslozhnyat' ne nuhzno, nuzhno chtoby byl prostoi access so storony uchitelya i uchenika.
-// v0.0.1: vozmozhnost' ispolozvoat' AI dlya studenta v obuchenii nekoi temy.
-
-// how to send system message on every session start?
-// need to push the generated system message to the list, so it can be accessed when retrieving chat history
-// I need to push it only once
-// so basically I push it when student starts a session.
